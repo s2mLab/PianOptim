32224237dfcf719a6e20e237cc7159a649d91900
Mathildesm <mathilde.desaintmeloir@gmail.com>
2022-09-06 14:43:05 +0000

    Hide the ground

--- MATHILDE_AFIICHER_BIOVIZ.py
+++ MATHILDE_AFIICHER_BIOVIZ.py
@@ -4,7 +4,7 @@ import bioviz
 
 model_path = "MATHILDE_Piano_final_version_with_pelvis.bioMod"
 
-b = bioviz.Viz(model_path)
+b = bioviz.Viz(model_path, show_floor=False, show_global_ref_frame=False)
 b.exec()
 
 
