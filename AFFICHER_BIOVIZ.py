a32295d236580bc027f72e6e38818db81b40be61
Mathildesm <mathilde.desaintmeloir@gmail.com>
2022-09-20 10:32:42 -0400

    No important changement

--- AFFICHER_BIOVIZ.py
+++ AFFICHER_BIOVIZ.py
@@ -1,8 +1,7 @@
 
 import bioviz
 
-
-model_path = "Piano.bioMod"
+model_path = "1D_Simulation/Example_Simulation_1D_with_impact.bioMod"
 
 b = bioviz.Viz(model_path, show_floor=False, show_global_ref_frame=False)
 b.exec()
