8960bc0f42ebed997fee8fd52bdee95d8ec83bd8
Mathildesm <mathilde.desaintmeloir@gmail.com>
2022-09-23 11:56:28 -0400

    Readaptation of datas to have results more coherant

--- Experimental_C3D_datas/Interaction_pianist_keys_velocity_time_experimental_C3D datas.py
+++ Experimental_C3D_datas/Interaction_pianist_keys_velocity_time_experimental_C3D datas.py
@@ -3,8 +3,10 @@ from ezc3d import c3d
 import numpy as np
 from matplotlib import pyplot as plt
 
+# Data Points
+#     # So each frame of the animation is printed, each frame has 75 datapoints (3 xyz, residual value, cameras value)
 
-c = c3d('/home/mickaelbegon/Documents/Stage_Mathilde/programation/PianOptim/Fichier_C3D/BasPreStaA.c3d')
+c = c3d('BasPreStaA.c3d')
 
 # General velocity vector
 POS = c['data']['points'][2][46][:]
@@ -34,35 +36,56 @@ plt.ylabel('Velocity (mm.s-1)')
 plt.show()
 
 # Max velocity vector for each attack (min bc the velocity is negative)
-DebDesc = [473,	879, 1286, 1676, 2048, 2411, 2777, 3179, 3551, 3927, 4305, 4667, 5042, 5434, 5832, 6178, 6549, 6918, 7270, 7592]
-DebutFondTouche = [485,	890, 1296, 1687, 2062, 2423, 2790, 3188, 3560, 3936, 4319, 4678, 5052, 5445, 5846, 6195, 6563, 6932, 7281, 7607]
-FinFondTouche = [492, 897, 1304, 1693, 2069, 2430, 2798, 3195, 3568, 3944, 4328, 4685, 5058, 5453, 5855, 6202, 6569, 6936, 7287, 7613]
+DebDesc = [473, 879, 1286, 1676, 2048, 2411, 2777, 3179, 3551, 3927, 4305, 4667, 5042, 5434, 5832, 6178, 6549, 6918, 7270, 7592]
+DebFondTouche = [485,	890, 1296, 1687, 2062, 2423, 2790, 3188, 3560, 3936, 4319, 4678, 5052, 5445, 5846, 6195, 6563, 6932, 7281, 7607]
+FinFondTouche = [498, 897, 1308, 1693, 2069, 2430, 2800, 3195, 3568, 3944, 4328, 4685, 5058, 5453, 5855, 6202, 6569, 6936, 7287, 7613]
+# With corrections
+# DebDesc = [879, 1676, 2048, 2411, 4305, 4667, 5042, 5832, 6178, 6549, 6918, 7270, 7592]
+# DebFondTouche = [890, 1687, 2062, 2423, 4319, 4678, 5052, 5846, 6195, 6563, 6932, 7281, 7607]
+# FinFondTouche = [897, 1693, 2069, 2430, 4328, 4685, 5058, 5855, 6202, 6569, 6936, 7287, 7613]
 
+# With corrections : correct_1 = delete 5 frames to every DebDesc, correct_2 = only attacks that have less than 10 f.
+DebDesc_correct_1 = []
+for i in range(20):
+    DebDesc_correct_1.append(DebDesc[i]+5)
+print(DebDesc_correct_1)
+
+DebDesc_correct_2 = []
+DebFondTouche_correct_2 = []
+FinFondTouche_correct_2 = []
+for i in range(20):
+    a = DebFondTouche[i] - DebDesc_correct_1[i]
+    if a < 10:
+        DebDesc_correct_2.append(DebDesc_correct_1[i])
+        DebFondTouche_correct_2.append(DebFondTouche[i])
+        FinFondTouche_correct_2.append(FinFondTouche[i])
+
+# max velocity vectors of each attack
 velocity_max_for_20 = []
-for i in range(19):
-    arr = np.array(velocity[DebDesc[i]:DebutFondTouche[i]])
-    velocity_max_for_20.append(max((arr)))
+for i in range(18):
+    arr = np.array(velocity[DebDesc_correct_2[i]:DebFondTouche_correct_2[i]])
+    velocity_max_for_20.append(max(arr))
 
 # Average of max velocity vectors of each attack
-average_velocity_max_20_notes = sum(velocity_max_for_20)/20
+average_velocity_max_20_notes = sum(velocity_max_for_20)/18
 print("Average of max velocity vectors of each attack : " + str(average_velocity_max_20_notes) + " mm.s-1")
 
 
 # Average of the time in s during the attack
 time_in_s_pushing = []
-for i in range(19):
-    time_in_s_pushing.append((DebutFondTouche[i]-DebDesc[i])/150)
+for i in range(18):
+    time_in_s_pushing.append((DebFondTouche_correct_2[i]-DebDesc_correct_2[i])/150)
 
-average_time_in_s_pushing = sum(time_in_s_pushing)/20
+average_time_in_s_pushing = sum(time_in_s_pushing)/18
 print("Average of the time during the attack : " + str(average_time_in_s_pushing*1000) + " ms")
 
 
 # Average of the time in s in Fondetouche
 time_in_s_Fonddetouche = []
-for i in range(19):
-    time_in_s_Fonddetouche.append((FinFondTouche[i]-DebutFondTouche[i])/150)
+for i in range(18):
+    time_in_s_Fonddetouche.append((FinFondTouche_correct_2[i]-DebFondTouche_correct_2[i])/150)
 
-average_time_in_s_Fonddetouche = sum(time_in_s_Fonddetouche)/20
+average_time_in_s_Fonddetouche = sum(time_in_s_Fonddetouche)/18
 print("Average of the time in Fondedetouche : " + str(average_time_in_s_Fonddetouche*1000) + " ms")
 
 
