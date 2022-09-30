d3eda2e4f4d470152df716eb55ab797ec2605ae0
Mathildesm <mathilde.desaintmeloir@gmail.com>
2022-09-30 17:14:53 -0400

    test to show defferent bioviz

--- AFFICHER_BIOVIZ.py
+++ AFFICHER_BIOVIZ.py
@@ -1,9 +1,11 @@
 
 import bioviz
 
-model_path = "1D_Simulation/Example_Simulation_1D_with_impact.bioMod"
+model_path = "/home/mickaelbegon/Documents/Stage_Mathilde/programation/PianOptim/0: On going/1D_Simulation/1_key_Simulation_hand_with_impact.bioMod"
 
-b = bioviz.Viz(model_path, show_floor=False, show_global_ref_frame=False)
+b = bioviz.Viz(model_path, markers_size=0.0010, contacts_size=0.0010, show_floor=False,
+                show_segments_center_of_mass=True, show_global_ref_frame=True,
+                show_local_ref_frame=True,)
 b.exec()
 
 
