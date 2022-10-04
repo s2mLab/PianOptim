97c2a47cd826556ac22b3dbe166ff3becab79d5f
Mathildesm <mathilde.desaintmeloir@gmail.com>
2022-10-04 17:18:58 -0400

    Display test

--- AFFICHER_BIOVIZ.py
+++ AFFICHER_BIOVIZ.py
@@ -1,11 +1,11 @@
 
 import bioviz
 
-model_path = "/home/mickaelbegon/Documents/Stage_Mathilde/programation/PianOptim/0: On going/1D_Simulation/1_key_Simulation_hand_with_impact.bioMod"
+model_path = "/home/mickaelbegon/Documents/Stage_Mathilde/programation/PianOptim/0: On going/2D_Simulation/2_keys_Simulation_hand_with_impact.bioMod"
 
 b = bioviz.Viz(model_path, markers_size=0.0010, contacts_size=0.0010, show_floor=False,
-                show_segments_center_of_mass=True, show_global_ref_frame=True,
-                show_local_ref_frame=True,)
+                show_segments_center_of_mass=False, show_global_ref_frame=True,
+                show_local_ref_frame=False,)
 b.exec()
 
 
