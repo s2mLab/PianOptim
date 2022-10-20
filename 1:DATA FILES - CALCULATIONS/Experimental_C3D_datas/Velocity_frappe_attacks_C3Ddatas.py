b01c23c4d109d5d3a6930d78c42f2fa9fd2a4987
Mathilde <mathilde.desaintmeloir@gmail.com>
2022-10-20 09:21:08 -0400

    Calculation of profile velocity for pressed and strucked attack

--- 1:DATA FILES - CALCULATIONS/Experimental_C3D_datas/Velocity_frappe_attacks_C3Ddatas.py
+++ 1:DATA FILES - CALCULATIONS/Experimental_C3D_datas/Velocity_frappe_attacks_C3Ddatas.py
@@ -21,67 +21,166 @@ velocity = [ele for ele in velocity]
 print(velocity)
 # Explanations :
 # 2/150 bc we want the velocity between i+1 et i-1 frame
-# We take the absolute velocity values
 
 # Plot the position depending on the time
-T = (np.linspace(0, (len(POS)/150), num=(len(POS))))
+Tsec = (np.linspace(0, (len(POS)/150), num=(len(POS))))
+Tframe = (np.linspace(0, (len(POS)), num=(len(POS))))
+T = Tframe
+
 plt.plot(T, POS, 'tab:red')
 plt.title("Positions of the middle finger marker for 18 attacks  \n")
-plt.xlabel('Time (s)')
+plt.xlabel('Frames from 0 to 10 000')
 plt.ylabel('Position (mm)')
+plt.grid(which='major', axis='x', color='black', linewidth=0.1)
+plt.xticks(np.arange(0, 10000, step=50))
+
+plt.axhline(y=0, color='black', linestyle='-')
+
+plt.axvline(x=898, color='b', linestyle='-')
+plt.axvline(x=956, color='b', linestyle='-')
+plt.axvline(x=960, color='b', linestyle='-')
+plt.axvline(x=967, color='b', linestyle='-')
+
+plt.axvline(x=1242, color='b', linestyle='-')
+plt.axvline(x=1289, color='b', linestyle='-')
+plt.axvline(x=1293, color='b', linestyle='-')
+plt.axvline(x=1302, color='b', linestyle='-')
+
 plt.show()
 
 # Plot the velocity vector depending on the time
-T = (np.linspace(0, (len(POS)/150)-3, num=(len(POS)-3)))
+Tsec = (np.linspace(0, (len(POS)/150)-3, num=(len(POS)-3)))
+Tframe = (np.linspace(0, (len(POS))-3, num=(len(POS)-3)))
+T = Tframe
+
 plt.plot(T, velocity, 'tab:red')
 plt.title("Velocity of the middle finger marker for 18 attacks \n")
-plt.xlabel('Time (s)')
+plt.xlabel('Frame from 0 to 10 000')
 plt.ylabel('Velocity (mm.s-1)')
+plt.grid(which='major', axis='x', color='black', linewidth=0.1)
+plt.xticks(np.arange(0, 10000, step=50))
+
+plt.axhline(y=0, color='black', linestyle='-')
+
+plt.axvline(x=898, color='b', linestyle='-')
+plt.axvline(x=956, color='b', linestyle='-')
+plt.axvline(x=960, color='b', linestyle='-')
+plt.axvline(x=967, color='b', linestyle='-')
+
+plt.axvline(x=1242, color='b', linestyle='-')
+plt.axvline(x=1289, color='b', linestyle='-')
+plt.axvline(x=1293, color='b', linestyle='-')
+plt.axvline(x=1302, color='b', linestyle='-')
 plt.show()
+
+T = Tsec
 
 # Data's, number of frames for each attacks
 DebDesc = [898,	  1242,    1578,	1916,	2246,	2568,	2903,	3225,	3572,	3912,	4254,	4570,	4908,	5235,	5582,	5908,	6243,	6582]
+Touched_key = [956, 1289, 1623, 1958, 2295, 2616, 2950, 3277, 3612, 3957, 4295, 4622, 4957, 5276, 5616, 5942, 6285, 6625]
 DebFondTouche = [960,   1293,	1626,	1962,	2298,	2619,	2954,	3281,	3616,	3961,	4299,	4627,	4961,	5280,	5620,	5946,	6289,	6629]
 FinFondTouche = [967,   1302,	1635,	1970,	2309,	2628,	2962,	3290,	3624,	3971,	4307,	4636,	4969,	5288,	5629,	5956,	6298,	6637]
 
-# # # Average of velocity of all attacks for each frame during the attack
+# # Average of velocity of all attacks for each frame between the beginning and the touched_key
 # All attacks between 0 and 100 sec
-# Interpolation done for 20 point
-velocity_each_attack_arrays = []
+#
+velocity_each_attack_arrays = np.zeros((20, 46))
 for i in range(18):
-    T = (np.linspace(0, 100, num=len(velocity[DebDesc[i]: DebFondTouche[i]])))
-    y = velocity[DebDesc[i]: DebFondTouche[i]]
-    n = len(velocity[DebDesc[i]: DebFondTouche[i]])
+    T = (np.linspace(0, 100, num=len(velocity[DebDesc[i]: Touched_key[i]])))
+    y = velocity[DebDesc[i]: Touched_key[i]]
+    n = len(velocity[DebDesc[i]: Touched_key[i]])
     f = interpolate.interp1d(T, y)
-    Tnew = np.linspace(0, 100, 50)
+    Tnew = np.linspace(0, 100, 46)
     interpolated = f(Tnew)
     plt.plot(Tnew, interpolated, '.')
-    velocity_each_attack_arrays.append(interpolated)
+    velocity_each_attack_arrays[i, :] = interpolated
 
 
-plt.title("Velocity profile of the middle finger marker for each attack \n")
+plt.title("Velocity profile for each attack between the beginning and the key \n")
 plt.xlabel('Time (s)')
 plt.ylabel('Velocity (mm.s-1)')
 plt.show()
-print("20 velocities of 18 attacks : ", velocity_each_attack_arrays)
+print("46 velocities of 18 attacks : ", velocity_each_attack_arrays)
+
 # Average of all attacks at each time step
-average_velocities_profile = []
-a = []
-for i in range(50):
-    for j in range(18):
-        a.append(velocity_each_attack_arrays[j][i])
-    average_velocities_profile.append(sum(a) / 50)
-    a.clear()
-print("average_velocities_profile", average_velocities_profile)
+average_velocities_profile = np.mean(velocity_each_attack_arrays, axis=0)
+average_velocities_profile = average_velocities_profile
+Tnew = np.linspace(0, 100, 46)
 
-Tnew = np.linspace(0, 100, 50)
+print("average_velocities_profile : ", average_velocities_profile)
+
 plt.plot(Tnew, average_velocities_profile, ".")
-plt.title("Average of velocity profiles of the middle finger marker of 18 attacks \n")
+plt.title("Average of velocity profiles between the beginning and the key \n")
 plt.xlabel('Time (s)')
 plt.ylabel('Velocity (mm.s-1)')
 plt.show()
 
-#  FinRise for each attack
+# # # Average of velocity of all attacks for each frame between the touched_key and the fond_de_touche
+# All attacks between 0 and 100 sec
+# Interpolation done for 20 point
+velocity_each_attack_arrays2 = np.zeros((20, 5))
+for i in range(18):
+    T2 = (np.linspace(0, 100, num=len(velocity[Touched_key[i]: DebFondTouche[i]])))
+    y2 = velocity[Touched_key[i]: DebFondTouche[i]]
+    n2 = len(velocity[Touched_key[i]: DebFondTouche[i]])
+    f2 = interpolate.interp1d(T2, y2)
+    Tnew2 = np.linspace(0, 100, 5)
+    interpolated = f2(Tnew2)
+    plt.plot(Tnew2, interpolated, '.')
+    velocity_each_attack_arrays2[i, :] = interpolated
+
+
+plt.title("Velocity profile for each attack between the key and the fond_de_touche \n")
+plt.xlabel('Time (s)')
+plt.ylabel('Velocity (mm.s-1)')
+plt.show()
+print("5 velocities of 18 attacks : ", velocity_each_attack_arrays2)
+
+# Average of all attacks at each time step
+average_velocities_profile2 = np.mean(velocity_each_attack_arrays2, axis=0)
+Tnew2 = np.linspace(0, 100, 5)
+
+print("average_velocities_profile2 : ", average_velocities_profile2)
+
+plt.plot(Tnew2, average_velocities_profile2, ".")
+plt.title("Average of velocity profiles between the key and the fond_de_touche \n")
+plt.xlabel('Time (s)')
+plt.ylabel('Velocity (mm.s-1)')
+plt.show()
+
+##### FELIPE VALUES #####
+# # # For jeu_frappe during the attack inside the key
+
+average_velocities_profile2 = [-0.698417100906372, -0.474601301515033, -0.368024758139809, -0.357349785081633, -0.367995643393795, -0.277969583506421, 0]
+Tnew2 = np.linspace(0, 100, 7)
+
+print("average_velocities_profile_7_nodes : ", average_velocities_profile2)
+
+plt.plot(Tnew2, average_velocities_profile2, ".")
+plt.title("Jeu frappe \n \n Average of velocity profiles between the key and the fond_de_touche \n")
+plt.xlabel('Time (s)')
+plt.ylabel('Velocity (m.s-1)')
+plt.show()
+
+# # # To find the value at 75% of the attack inside the key
+T = (np.linspace(0, 100, num=7))
+average_velocities_profile2 = [-0.698417100906372, -0.474601301515033, -0.368024758139809, -0.357349785081633, -0.367995643393795, -0.277969583506421, 0]
+n = len(average_velocities_profile2)
+f = interpolate.interp1d(T, average_velocities_profile2)
+Tnew3 = np.linspace(0, 100, 5)
+interpolated = f(Tnew3)
+plt.plot(Tnew3, interpolated, '.')
+
+print("average_velocities_profile_5_nodes_75% : ", interpolated)
+
+plt.axvline(x=75, color='black', linestyle='-')
+plt.plot(Tnew3, interpolated, ".")
+plt.title("Jeu frappe \n \n Average of velocity profiles to find the velocity at 75% \n")
+plt.xlabel('Time (s)')
+plt.ylabel('Velocity (m.s-1)')
+plt.show()
+
+# # # FinRise for each attack
 FinRise = []
 for i in range(18):
     FinRise .append(FinFondTouche[i]+10)
--- 1:DATA FILES - CALCULATIONS/Experimental_C3D_datas/Velocity_pressed_attacks_C3Ddatas.py
+++ 1:DATA FILES - CALCULATIONS/Experimental_C3D_datas/Velocity_pressed_attacks_C3Ddatas.py
@@ -21,57 +21,127 @@ velocity = [ele for ele in velocity]
 print(velocity)
 # Explanations :
 # 2/150 bc we want the velocity between i+1 et i-1 frame
-# We take the absolute velocity values
 
 # Plot the position depending on the time
-T = (np.linspace(0, (len(POS)/150), num=(len(POS))))
+Tsec = (np.linspace(0, (len(POS)/150), num=(len(POS))))
+Tframe = (np.linspace(0, (len(POS)), num=(len(POS))))
+T = Tframe
+
 plt.plot(T, POS, 'tab:red')
-plt.title("Position for 20 attacks  \n")
-plt.xlabel('Time (s)')
+plt.title("Positions of the middle finger marker for 20 attacks  \n")
+plt.xlabel('Frames from 0 to 10 000')
 plt.ylabel('Position (mm)')
+plt.grid(which='major', axis='x', color='black', linewidth=0.1)
+plt.xticks(np.arange(0, 10000, step=50))
+
+plt.axhline(y=0, color='black', linestyle='-')
+
+plt.axvline(x=2048, color='b', linestyle='-')
+plt.axvline(x=2062, color='b', linestyle='-')
+plt.axvline(x=2069, color='b', linestyle='-')
+
+plt.axvline(x=2411, color='b', linestyle='-')
+plt.axvline(x=2423, color='b', linestyle='-')
+plt.axvline(x=2430, color='b', linestyle='-')
+# +5, -2
 plt.show()
 
 # Plot the velocity vector depending on the time
-T = (np.linspace(0, (len(POS)/150)-3, num=(len(POS)-3)))
+Tsec = (np.linspace(0, (len(POS)/150)-3, num=(len(POS)-3)))
+Tframe = (np.linspace(0, (len(POS))-3, num=(len(POS)-3)))
+T = Tframe
+
 plt.plot(T, velocity, 'tab:red')
-plt.title("Velocity Vector for 20 attacks \n")
-plt.xlabel('Time (s)')
+plt.title("Velocity of the middle finger marker for 20 attacks \n")
+plt.xlabel('Frames from 0 to 10 000')
 plt.ylabel('Velocity (mm.s-1)')
+plt.grid(which='major', axis='x', color='black', linewidth=0.1)
+plt.xticks(np.arange(0, 10000, step=50))
+
+plt.axhline(y=0, color='black', linestyle='-')
+
+plt.axvline(x=2048, color='b', linestyle='-')
+plt.axvline(x=2062, color='b', linestyle='-')
+plt.axvline(x=2069, color='b', linestyle='-')
+
+plt.axvline(x=2411, color='b', linestyle='-')
+plt.axvline(x=2423, color='b', linestyle='-')
+plt.axvline(x=2430, color='b', linestyle='-')
+
 plt.show()
+
+T = Tsec
 
 # Data's, number of frames for each attacks
 DebDesc = [473, 879, 1286, 1676, 2048, 2411, 2777, 3179, 3551, 3927, 4305, 4667, 5042, 5434, 5832, 6178, 6549, 6918, 7270, 7592]
 DebFondTouche = [485, 890, 1296, 1687, 2062, 2423, 2790, 3188, 3560, 3936, 4319, 4678, 5052, 5445, 5846, 6195, 6563, 6932, 7281, 7607]
 FinFondTouche = [498, 897, 1308, 1693, 2069, 2430, 2800, 3195, 3568, 3944, 4328, 4685, 5058, 5453, 5855, 6202, 6569, 6936, 7287, 7613]
 
-## Average of velocity of all atacks for each frame during the attack
-# All attacks between 0 and 100 sec.
-# And interpolation for 20 points
+# # # # Average of velocity of all attacks for each frame during the attack
+# # All attacks between 0 and 100 sec
+# # Interpolation done for 20 point
+# # velocity_each_attack_arrays = []
+# velocity_each_attack_arrays = np.zeros((20, 8))
+# for i in range(20):
+#     T = (np.linspace(0, 100, num=len(velocity[DebDesc[i]: DebFondTouche[i]])))
+#     y = velocity[DebDesc[i]: DebFondTouche[i]]
+#     n = len(velocity[DebDesc[i]: DebFondTouche[i]])
+#     f = interpolate.interp1d(T, y)
+#     Tnew = np.linspace(0, 100, 8)
+#     interpolated = f(Tnew)
+#     plt.plot(Tnew, interpolated, '.')
+#     velocity_each_attack_arrays[i, :] = interpolated
+#
+#
+# plt.title("Velocity profile of the middle finger marker for each attack \n")
+# plt.xlabel('Time (s)')
+# plt.ylabel('Velocity (mm.s-1)')
+# plt.show()
+# print("20 velocities of 20 attacks : ", velocity_each_attack_arrays)
+#
+# # Average of all attacks at each time step
+# average_velocities_profile = np.mean(velocity_each_attack_arrays, axis=0)
+# average_velocities_profile = average_velocities_profile
+# Tnew = np.linspace(0, 100, 8)
+#
+# print("average_velocities_profile : ", average_velocities_profile)
+#
+# plt.plot(Tnew, average_velocities_profile, ".")
+# plt.title("Average of velocity profiles of the middle finger marker of 20 attacks \n")
+# plt.xlabel('Time (s)')
+# plt.ylabel('Velocity (mm.s-1)')
+# plt.show()
 
-for i in range(20):
-    T = (np.linspace(0, 100, num=len(velocity[DebDesc[i]: DebFondTouche[i]])))
-    y = velocity[DebDesc[i]: DebFondTouche[i]]
-    n = len(velocity[DebDesc[i]: DebFondTouche[i]])
-    f = interpolate.interp1d(T, y, kind=5, fill_value='extrapolate')
-    #interp_func = interp1d(T, y)
-    #newarr = interp_func(np.arange(0, 100, 5))
-    # df_interpol = df.groupby('house') \
-    #     .resample('D') \
-    #     .mean()
-    # df_interpol['readvalue'] = df_interpol['readvalue'].interpolate()
-    # df_interpol.head(4)
+##### FELIPE VALUES #####
+# # # For jeu_presse during the attack inside the key
+average_velocities_profile2 = [0, -0.113772161006927, -0.180575996580578, -0.270097219830468, -0.347421549388341, -0.290588704744975, -0.0996376128423782, 0]
+Tnew2 = np.linspace(0, 100, 8)
 
-plt.plot(T, f(T), "r")
-# plt.plot(T, y, 'ro')
-# plt.plot(T, y, 'b')
-plt.title("Velocity Vector for each attacks \n")
+print("average_velocities_profile_8_nodes : ", average_velocities_profile2)
+
+plt.plot(Tnew2, average_velocities_profile2, ".")
+plt.title("Jeu presse \n \n Average of velocity profiles between the key and the fond_de_touche \n")
 plt.xlabel('Time (s)')
-plt.ylabel('Velocity (mm.s-1)')
+plt.ylabel('Velocity (m.s-1)')
 plt.show()
 
+# # # To find the value at 75% of the attack inside the key
+T = (np.linspace(0, 100, num=8))
+average_velocities_profile2 = [0, -0.113772161006927, -0.180575996580578, -0.270097219830468, -0.347421549388341, -0.290588704744975, -0.0996376128423782, 0]
+n = len(average_velocities_profile2)
+f = interpolate.interp1d(T, average_velocities_profile2)
+Tnew3 = np.linspace(0, 100, 5)
+interpolated = f(Tnew3)
+plt.plot(Tnew3, interpolated, '.')
 
-# Velocitiies of the 9th attack that we use for the pushed attack.
-print('Velocity_attack_9 : ', velocity[3551:3560])
+print("average_velocities_profile_5_nodes_75% : ", interpolated)
+
+plt.axvline(x=75, color='black', linestyle='-')
+plt.plot(Tnew3, interpolated, ".")
+plt.title("Jeu presse \n \n Average of velocity profiles to find the velocity at 75% \n")
+plt.xlabel('Time (s)')
+plt.ylabel('Velocity (m.s-1)')
+plt.show()
 
 # With corrections : correct_1 = delete 5 frames to every DebDesc, correct_2 = only attacks that have less than 10 f.
 DebDesc_correct_1 = []
@@ -129,3 +199,14 @@ for i in range(18):
 
 average_time_in_s_Fonddetouche = sum(time_in_s_Fonddetouche)/18
 print("Average of the time in Fondedetouche : " + str(average_time_in_s_Fonddetouche*1000) + " ms")
+
+##### FELIPE VALUES #####
+# For jeu_presse during the attack inside the key
+# 0
+# -113.772161006927
+# -180.575996580578
+# -270.097219830468
+# -347.421549388341
+# -290.588704744975
+# -99.6376128423782
+# 0
