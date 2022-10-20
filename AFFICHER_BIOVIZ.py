406ff52b0f12db52faa520f024504783cfe95ddb
Mathilde <mathilde.desaintmeloir@gmail.com>
2022-10-20 09:16:30 -0400

    Test (showing bioviz)

--- AFFICHER_BIOVIZ.py
+++ AFFICHER_BIOVIZ.py
@@ -1,10 +1,10 @@
 
 import bioviz
 
-model_path = "/home/lim/Documents/Stage Mathilde/PianOptim/0:On going/Piano_with_hand_and_key/Piano_with_hand_and_keys.bioMod"
+model_path = "/0:On going/5:FINAL_Piano_with_hand_and_key_frappe_/Piano_with_hand_and_keys.bioMod"
 
 b = bioviz.Viz(model_path, markers_size=0.0020, contacts_size=0.0020, show_floor=False,
-                show_segments_center_of_mass=False, show_global_ref_frame=False,
+                show_segments_center_of_mass=False, show_global_ref_frame=True,
                 show_local_ref_frame=False,)
 b.exec()
 
