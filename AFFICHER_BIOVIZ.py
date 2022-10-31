a7104df3a65c909839830122eed6e86b6422c79c
Mathilde <mathilde.desaintmeloir@gmail.com>
2022-10-31 14:09:46 -0400

    None important modifs and tests

--- AFFICHER_BIOVIZ.py
+++ AFFICHER_BIOVIZ.py
@@ -1,11 +1,11 @@
 
 import bioviz
 
-model_path = "/0:On going/5:FINAL_Piano_with_hand_and_key_frappe_/Piano_with_hand_and_keys.bioMod"
+model_path = "/home/lim/Documents/Stage Mathilde/PianOptim/0:On_going/5:FINAL_Squeletum_hand_finger_2_keys/Frappe/4_phases/Squeletum_hand_finger_3D_2_keys_octave_LA_frappe.bioMod"
 
-b = bioviz.Viz(model_path, markers_size=0.0020, contacts_size=0.0020, show_floor=False,
+b = bioviz.Viz(model_path, markers_size=0.00150, contacts_size=0.00150, show_floor=False,
                 show_segments_center_of_mass=False, show_global_ref_frame=True,
-                show_local_ref_frame=False,)
+                show_local_ref_frame=True,)
 b.exec()
 
 
